4f086910c6b605cf22877993616ddb13aa3bd557
nicolanzi1 <nicolanzi@gmail.com>
2020-12-11 18:45:38 +0000

    Wrote the header and footer components, imported react-bootstrap, added the bootstrap theme from bootswatch to the components folder, and the minified font awesome icons to the index.html file

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Fathom3</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/jokes">
+                <i className="fas fa-smile-wink"></i> Jokes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
